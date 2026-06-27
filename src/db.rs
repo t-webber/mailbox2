87078a7448f6b53ae3d543355a88586314cb831e
Tom Webber <t.c.w.webber@gmail.com>
2026-06-27 12:00:00 +0100

    feat(db): initialise database

---
+++ src/db.rs
@@ -0,0 +1,25 @@
+use std::path::Path;
+
+use sqlx::SqlitePool;
+use sqlx::sqlite::SqliteConnectOptions;
+
+pub async fn setup_db(db_url: &Path) -> Result<SqlitePool, sqlx::Error> {
+    let options = SqliteConnectOptions::new()
+        .create_if_missing(true)
+        .filename(db_url);
+    let pool = SqlitePool::connect_with(options).await?;
+
+    sqlx::query(
+        "create table if not exists message (
+            uid integer primary key,
+            subject text not null,
+            from_email text not null,
+            date text not null,
+            body text
+         );",
+    )
+    .execute(&pool)
+    .await?;
+
+    Ok(pool)
+}
