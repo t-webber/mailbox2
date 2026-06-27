8bccf0c47edc50bf51a66c5eb54da201504fedcc
Tom Webber <t.c.w.webber@gmail.com>
2026-06-27 12:00:00 +0100

    feat(db): initialise database

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::Path;
+
+use crate::db::setup_db;
+
+mod db;
+
+#[tokio::main]
+async fn main() {
+    let _ = setup_db(Path::new("db.sqlite")).await.unwrap();
 }
