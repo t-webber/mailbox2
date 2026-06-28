bebc08a9d0ff51c70b01933e80490addee41ceca
Tom Webber <t.c.w.webber@gmail.com>
2026-06-28 12:00:00 +0100

    feat(imap): fetch message headers

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 use dotenv::dotenv;
 
 use crate::db::setup_db;
-use crate::imap::connect_imap;
+use crate::imap::{connect_imap, fetch_headers};
 
 mod db;
 mod imap;
@@ -14,12 +14,13 @@ async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     dotenv()?;
     let _ = setup_db(Path::new("db.sqlite")).await?;
-    let _ = connect_imap(
+    let mut session = connect_imap(
         &var("MBX_DOMAIN")?,
         var("MBX_PORT")?.parse()?,
         &var("MBX_USER")?,
         &var("MBX_PASSWORD")?,
     )
     .await?;
+    fetch_headers(&mut session).await?;
     Ok(())
 }
