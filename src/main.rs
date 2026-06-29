4f5515c9cbf2f1298440316947ddc60fbca9962e
Tom Webber <t.c.w.webber@gmail.com>
2026-06-29 12:00:00 +0100

    chore(ci): add clippy configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+//! Mailbox crate.
+//!
+//! Crate to ease managing a mailbox, including fetching email bodies, checking
+//! for new messages not yet pulled, and sending out new email.
+
+/// Handles database connections.
+mod db;
+/// Handles interactions with the IMAP protocol.
+mod imap;
+/// Decodes the encoded subjects.
+mod subject_decoder;
+#[cfg(test)]
+mod test_subject_decoder;
+
 use std::env::var;
 use std::path::Path;
 
@@ -6,17 +20,11 @@ use dotenv::dotenv;
 use crate::db::setup_db;
 use crate::imap::{connect_imap, fetch_headers};
 
-mod db;
-mod imap;
-mod subject_decoder;
-#[cfg(test)]
-mod test_subject_decoder;
-
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     dotenv()?;
-    let _ = setup_db(Path::new("db.sqlite")).await?;
+    drop(setup_db(Path::new("db.sqlite")).await?);
     let mut session = connect_imap(
         &var("MBX_DOMAIN")?,
         var("MBX_PORT")?.parse()?,
