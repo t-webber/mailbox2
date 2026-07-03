6dc096c6232177d67a0c27f376e803923fc51f1f
Tom Webber <t.c.w.webber@gmail.com>
2026-07-03 12:00:00 +0100

    feat(gui): add iced boilerplate

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,10 +1,21 @@
 //! Main application binary that runs everything that is needed.
 
+use std::env::args;
+
+use color_eyre::Result;
 use mailbox_cli::cli;
+use mailbox_gui::GuiApp;
 
 /// Runs the CLI application.
-#[tokio::main]
-async fn main() -> color_eyre::Result<()> {
+fn main() -> Result<()> {
     color_eyre::install()?;
+    match args().nth(1) {
+        Some(arg) if arg == "cli" => tokio_cli(),
+        _ => GuiApp::run(),
+    }
+}
+
+#[tokio::main]
+async fn tokio_cli() -> Result<()> {
     cli().await
 }
